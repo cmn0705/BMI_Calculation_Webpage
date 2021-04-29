e1ed02df490d6133559ce465e42608524f592745
Chau <cmn0705@gmail.com>
2021-04-28 23:51:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+*Tags: html, css, javascript*
+
+**BMI Calculator web application**
+
+[Paris WebPage](https://cmn0705.github.io/BMI_Calcuation_Webpage/BMI.htm)
---
+++ scripts.js
@@ -0,0 +1,5 @@
+function cal(){
+    w=document.getElementById("w").value;
+    h=document.getElementById("h").value;
+	document.getElementById("output").innerHTML =703*w/h/h;
+}
